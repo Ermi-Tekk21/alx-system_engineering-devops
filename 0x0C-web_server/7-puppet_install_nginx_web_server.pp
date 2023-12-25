1ee541142ceafcaa4ad77dc5a15d5e06c613e159
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-12-25 09:46:23 +0300

    transfer file to server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,14 @@
-# Install Nginx web server (w/ Puppet)
+# Automating project requirements using Puppet
+
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
+file_line { 'install':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
 }
 
 file { '/var/www/html/index.html':
